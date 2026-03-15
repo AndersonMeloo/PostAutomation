ef775ae6fdffc64fd44a574d30912737e2f4d981
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-15 17:13:03 -0300

    feat: Importando UseGuards(AuthGuard) Para Proteger as Rotas

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/common/guards/jwt-auth.guards';
 
 @Controller('users')
 export class UsersController {
@@ -21,35 +23,41 @@ export class UsersController {
   }
 
   // Buscando todos os Usuários
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
   // Buscando todos os E-mails
+  @UseGuards(AuthGuard)
   @Get('emails')
   findAllEmails() {
     return this.usersService.findAllEmails();
   }
 
   // Buscando Usuário por E-mail
+  @UseGuards(AuthGuard)
   @Get('email/:email')
   findUserByEmail(@Param('email') email: string) {
     return this.usersService.findUserByEmail(email);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   // Atualizar Dados do Usuário
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   // Deletar um Usuário
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
