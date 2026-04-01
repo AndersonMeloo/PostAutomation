618b7669caa14e0d9dacee760072cb0758e5aa12
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-01 20:43:36 -0300

    refactor(niches-controller): Usar uuid string e metodos assincronos - Troca de id numérico para string em find/update/remove

--- backend/src/modules/niches/niches.controller.ts
+++ backend/src/modules/niches/niches.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { NichesService } from './niches.service';
 import { CreateNichDto } from './dto/create-nich.dto';
 import { UpdateNichDto } from './dto/update-nich.dto';
@@ -8,27 +16,27 @@ export class NichesController {
   constructor(private readonly nichesService: NichesService) {}
 
   @Post()
-  create(@Body() createNichDto: CreateNichDto) {
+  async create(@Body() createNichDto: CreateNichDto) {
     return this.nichesService.create(createNichDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.nichesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.nichesService.findOne(+id);
+    return this.nichesService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateNichDto: UpdateNichDto) {
-    return this.nichesService.update(+id, updateNichDto);
+    return this.nichesService.update(id, updateNichDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.nichesService.remove(+id);
+    return this.nichesService.remove(id);
   }
 }
