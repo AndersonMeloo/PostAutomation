7cb76f3e61058af29652832b8ae0e4dcc3409df9
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-02 23:49:34 -0300

    feat(auth) Criando Guard Google com escopo de uploud do youtube

---
+++ backend/src/modules/auth/guards/google-auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import type { AuthenticateOptionsGoogle } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleAuthGuard extends AuthGuard('google') {
+  getAuthenticateOptions(): AuthenticateOptionsGoogle {
+    return {
+      scope: [
+        'email',
+        'profile',
+        'https://www.googleapis.com/auth/youtube.upload',
+      ],
+      accessType: 'offline',
+      prompt: 'consent',
+      includeGrantedScopes: true,
+    };
+  }
+}
