9aadca63997a6801c5dd42de0176542e033f2d5b
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-06-29 00:23:04 -0300

    fix: configure cors production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,21 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const allowedOrigins = [
-  //   process.env.FRONTEND_URL,
-  //   'https://post-automation-psi.vercel.app',
-  //   /^https:\/\/post-automation-.*\.vercel\.app$/,
-  //   'http://localhost:3000',
-  //   'http://localhost:3001',
-  //   'http://localhost:5173',
-  // ].filter(Boolean);
-
   app.enableCors({
-    // origin: allowedOrigins,
-    origin: true,
+    origin: [
+      'https://post-automation-psi.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:5173',
+    ],
     credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
